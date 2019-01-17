4a7d830215a622dd7928b03a07ad6a24c3f5b59c
Gilbert Torchon <gilbert@noukod.com>
2019-01-17 19:58:11 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,8 +15,13 @@ class TweetsController < ApplicationController
   end
 
   get "/tweets/:id" do
-    @tweet=Tweet.find_by_id(@params[:id])
-    erb :"tweets/show_tweet"
+    if logged_in? 
+      @tweet=Tweet.find_by_id(@params[:id])
+      erb :"tweets/show_tweet"
+    else 
+      redirect "/login"
+    end
+  
   end
 
   post "/tweets" do
