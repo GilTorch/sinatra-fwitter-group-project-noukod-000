988a2dd997a05207ebac7cff9b00edd30f45a6dc
Gilbert Torchon <gilbert@noukod.com>
2019-01-17 20:06:26 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,7 +25,7 @@ class TweetsController < ApplicationController
   end
 
   post "/tweets" do
-    binding.ry
+    binding.pry
     @tweet=Tweet.create({content:@params[:tweet][:content]})
     if tweet.valid?
       current_user.tweets << @tweet
