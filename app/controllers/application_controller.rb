5feafb973c570d6366ea9882316104ed4b13499f
Gilbert Torchon <gilbert@noukod.com>
2019-01-17 19:43:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
       end
 
       def current_user
-        User.find_by_id(session[:id])
+        User.find_by_id(session[:user_id])
       end
   end
 
